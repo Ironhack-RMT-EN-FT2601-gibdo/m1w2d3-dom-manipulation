15bf28c6abbe311d27c1d5772561b15ce1e7809d
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2026-01-21 11:47:20 +0100

    dom manipulation done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/* 
 
 console.log(document) 
 
@@ -50,3 +50,61 @@ console.log(something)
 
 
 // querySelector is mainly used to find things in the whole document. BUT can be used to search things inside a specific node.
+*/
+
+let h1Node = document.querySelector("h1") // fist available h1
+
+let thirdH1Node = document.querySelector("#third-title")
+
+console.log(thirdH1Node)
+
+thirdH1Node.addEventListener("click", () => {
+  console.log("clicking")
+
+  thirdH1Node.innerText = "changing the DOM!"
+  h1Node.innerText = "this also changes!"
+
+})
+
+// button that increases the counter in the screen
+let incButtonNode = document.querySelector("#increment")
+let counterNode = document.querySelector("#count h2 span")
+
+incButtonNode.addEventListener("click", () => {
+  // ++
+  // counterNode.innerText = parseInt(counterNode.innerText) + 1
+  counterNode.innerText++
+})
+
+//* deleting nodes
+let deleteBtnNode = document.querySelector("#delete-btn")
+
+deleteBtnNode.addEventListener("click", () => {
+  deleteBtnNode.remove() // it does not destroy it. It simply removes it from the document.
+
+  // document.querySelector("body").remove()
+
+})
+
+//* adding new nodes
+let inputNode = document.querySelector("#name")
+let addBtnNode = document.querySelector("#add-form button")
+let ulNode = document.querySelector("#add-list")
+
+console.log(inputNode)
+
+addBtnNode.addEventListener("click", () => {
+
+  // extract the text from the input
+  let textFromUser = inputNode.value
+
+  // create the new li element
+  let newLiNode = document.createElement("li") // <li></li>
+  // add the text to the li
+  newLiNode.innerText = textFromUser
+  console.log(newLiNode)
+
+  // insert the li into the ul
+  ulNode.append(newLiNode)
+
+})
